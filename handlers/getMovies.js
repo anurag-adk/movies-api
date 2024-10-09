626ec97774fcc50ed6afa169c5f81fcd2d44e010
Anurag Adhikari <anuragadhikari5276@gmail.com>
2024-10-09 20:01:43 +0545

    Read Operation

--- handlers/getMovies.js
+++ handlers/getMovies.js
@@ -1,6 +1,13 @@
-const getMovies = (req, res) => {
+const mongoose = require("mongoose");
+
+const getMovies = async (req, res) => {
+  const movieModel = mongoose.model("movies");
+  const listMovies = await movieModel.find({
+    // rating: 8.8,
+  });
   res.status(200).json({
-    message: "All Movies",
+    // message: "All Movies",
+    movies: listMovies,
   });
 };
 
