89f6d1f750a01cf1dbb8922af1bbfd63fe74c0e4
Anurag Adhikari <anuragadhikari5276@gmail.com>
2024-10-10 16:20:13 +0545

    delete operation

--- handlers/deleteMovie.js
+++ handlers/deleteMovie.js
@@ -1,5 +1,32 @@
-const deleteMovie = (req, res) => {
+const mongoose = require("mongoose");
+
+const deleteMovie = async (req, res) => {
+  const movieModel = mongoose.model("movies");
+
+  // console.log(req.query);
+
+  const { id } = req.query;
+  try {
+    if (!id) throw "Id is missing";
+  } catch (err) {
+    res.status(400).json({
+      status: "Failed",
+      message: err,
+    });
+    return;
+  }
+
+  try {
+    await movieModel.deleteOne({ _id: id });
+  } catch (err) {
+    res.status(400).json({
+      status: "Failed",
+      message: err.message,
+    });
+  }
+
   res.status(200).json({
+    status: "Success",
     message: "Movie Deleted",
   });
 };
