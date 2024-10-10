e8dfb6379ae5d6f3486701bdf720d562d2b9052c
Anurag Adhikari <anuragadhikari5276@gmail.com>
2024-10-10 14:52:38 +0545

    update operation

--- handlers/updateMovie.js
+++ handlers/updateMovie.js
@@ -1,5 +1,45 @@
-const updateMovie = (req, res) => {
+const mongoose = require("mongoose");
+
+const updateMovie = async (req, res) => {
+  const movieModel = mongoose.model("movies");
+
+  // console.log(req.body);
+
+  const { _id, name, info, img, rating } = req.body;
+  try {
+    if (!_id) throw "Id is missing";
+  } catch (err) {
+    res.status(400).json({
+      status: "Failed",
+      message: err,
+    });
+    return;
+  }
+
+  try {
+    await movieModel.updateOne(
+      {
+        _id,
+      },
+      {
+        name,
+        info,
+        img,
+        rating,
+      },
+      {
+        runValidators: true,
+      }
+    );
+  } catch (err) {
+    res.status(400).json({
+      status: "Failed",
+      message: err.message,
+    });
+    return;
+  }
   res.status(200).json({
+    status: "Success",
     message: "Movie Updated",
   });
 };
